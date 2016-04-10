dd989cedf856e30a56c1e0e2d6721f41169e8f0b
duncan.runnacles <duncan.runnacles@thomasmackay.co.uk>
2016-04-10 13:33:22 +0100

    Added test data and the tests that require it.
    Includes a test_data folder under the tests folder that contains
    some test data for checking DataFileObject status and loading
    with real data.

---
+++ tests/test_datafileloader.py
@@ -0,0 +1,267 @@
+
+import unittest
+import os
+import hashlib
+
+from ship.tuflow.data_files import datafileloader
+from ship.tuflow.data_files.datafileobject import TmfDataObject, \
+                                BcDataObject, MatCsvDataObject, XsDataObject
+from ship.tuflow.tuflowfilepart import DataFile, GisFile
+from ship.data_structures.rowdatacollection import RowDataCollection
+from ship.data_structures.dataobject import DataTypes
+
+
+class DataFileLoaderTests(unittest.TestCase):
+    '''Tests the function and setup of the ADataFileComponent_OLD object and it's 
+    subclasses.
+    '''
+    
+    def setUp(self):
+        '''Setup any expensive variables that are needed for all tests.
+        '''
+        self.curd = os.getcwd()
+        self.comment_types = ['!', '#']
+        
+    
+    def test_extractInlineComment(self):
+        """Check that comments are extracted from line correctly"""
+
+        testline1 = '1,0.06,,,,,,,,,,! Open pasture'
+        testline2 = '1,0.06,,,,,,,,,,# Open pasture'
+        outline = '1,0.06,,,,,,,,,,'
+        outcomment = ' Open pasture'
+        
+        self.assertTupleEqual((outline, outcomment), datafileloader._extractInlineComment(testline1, self.comment_types), 'Inline comment extraction fail for "!"')
+        self.assertTupleEqual((outline, outcomment), datafileloader._extractInlineComment(testline2, self.comment_types), 'Inline comment extraction fail for "#"')
+        
+    
+    def test_hasCommentOnlyLine(self):
+        """Check that we can pick up lines with only comments on"""
+
+        testline1 = '! Some comment only line'
+        testline2 = '# Some comment only line 2'
+        testline3 = '1,0.06,,,,,,,,,,! Open pasture'
+        
+        self.assertEqual(testline1, datafileloader.hasCommentOnlyLine(testline1, self.comment_types), 'Comment only line fail for "!"')
+        self.assertEqual(testline2, datafileloader.hasCommentOnlyLine(testline2, self.comment_types), 'Comment only line fail for "#"')
+        self.assertFalse(datafileloader.hasCommentOnlyLine(testline3, self.comment_types), 'Not a comment only line fail')
+        
+    
+    def test_createXsDataFileObject_Shp(self):
+        """Loads a 1d cross section gis file and checks the contents get setup."""
+        
+        test_list = [['Section1.csv', 'Section2.csv', 'Section3.csv', 'Section4.csv'],
+                     ['xz', 'xz', 'xz', 'xz'],
+                     ['', '', '', ''],
+                     ['Section1', 'Section2', 'Section3', 'Section4'],
+                     ['', '', '', ''],
+                     ['', '', '', ''],
+                     ['', '', '', ''],
+                     ['', '', '', ''],
+                     ['', '', '', ''],
+                     [0.0, 0.0, 0.0, 0.0],
+                     [0.0, 0.0, 0.0, 0.0],
+                     [0.0, 0.0, 0.0, 0.0],
+                     [0, 1, 2, 3]
+                    ]
+        
+        # First test for Shapefile
+        gis_path = os.path.join(self.curd, r'tests\test_data\xs\1d_xs_test.shp')
+        hex_hash = hashlib.md5(gis_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = GisFile(1, gis_path, hex_hash, 3, 'Read MI Table Links')
+        gis = datafileloader.loadDataFile(datafile)
+    
+        self.assertIsInstance(gis, XsDataObject, 'Factory create XsDataObject fail - SHP')
+        self.assertEqual(hex_hash, gis.path_holder.hex_hash, 'Hex_hash equality fail - SHP')
+        self.assertEqual('Read MI Table Links', gis.path_holder.command, 'Command equality fail - SHP')
+        
+        row_list = gis.row_collection.getRowDataAsList()
+        self.assertListEqual(test_list, row_list, 'List contents not equal fail - SHP')
+        
+        # Then for MapInfo file
+        gis_path = os.path.join(self.curd, r'tests\test_data\xs\1d_xs_test.mif')
+        hex_hash = hashlib.md5(gis_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = GisFile(1, gis_path, hex_hash, 3, 'Read MI Table Links')
+        gis = datafileloader.loadDataFile(datafile)
+    
+        self.assertIsInstance(gis, XsDataObject, 'Factory create XsDataObject fail - MIF')
+        self.assertEqual(hex_hash, gis.path_holder.hex_hash, 'Hex_hash equality fail - MIF')
+        self.assertEqual('Read MI Table Links', gis.path_holder.command, 'Command equality fail - MIF')
+        
+        row_list = gis.row_collection.getRowDataAsList()
+        self.assertListEqual(test_list, row_list, 'List contents not equal fail - MIF')
+    
+    
+    def test_createBcDataFileObject(self):
+        """Loads a boundary condition csv file and checks that it is setup properly"""
+              
+        bc_path = os.path.join(self.curd, r'tests\test_data\bc_dbase\bc_dbase_test.csv')
+        hex_hash = hashlib.md5(bc_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, bc_path, hex_hash, 3, 'BC Database')
+        bc = datafileloader.loadDataFile(datafile)
+    
+        self.assertIsInstance(bc, BcDataObject, 'Factory create BcDataObject fail')
+        self.assertEqual(hex_hash, bc.path_holder.hex_hash, 'Hex_hash equality fail')
+        self.assertEqual('BC Database', bc.path_holder.command, 'Command equality fail')
+        
+        test_list = [['Name1', 'Name2', 'Name3'],
+                     ['Name1.csv', 'Name2.csv', 'Name2.csv'],
+                     ['time', 'time', 'time'],
+                     ['Downstream', 'Flow', 'Dummy'],
+                     ['', '', ''],
+                     ['', '', ''],
+                     ['', '', ''],
+                     ['', '', ''],
+                     ['', '', ''],
+                     ['Name', 'Source', 'Column1_or_Time', 'Column2_or_Value_or_ID', 
+                      'TimeAdd', 'ValueMult', 'ValueAdd', 'IMultF', 'IAddF'],
+                     [0, 1, 2]
+                    ]
+        
+        row_list = bc.row_collection.getRowDataAsList()
+        self.assertListEqual(test_list, row_list, 'List contents not equal fail')
+        
+    
+    def test_createTmfDataFileObject(self):
+        """Loads a .tmf file and checks that it is setup properly"""
+        
+        mat_path = os.path.join(self.curd, r'tests\test_data\Materials_TMF.tmf')
+        hex_hash = hashlib.md5(mat_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, mat_path, hex_hash, 3, 'Read Materials File')
+        mat = datafileloader.loadDataFile(datafile)
+    
+        self.assertIsInstance(mat, TmfDataObject, 'Factory create TmfDataObject fail')
+        self.assertEqual(hex_hash, mat.path_holder.hex_hash, 'Hex_hash equality fail')
+        self.assertEqual('Read Materials File', mat.path_holder.command, 'Command equality fail')
+        
+        test_list = [[1, 2, 3, 4, 5, 6, 7],
+                     [0.06, 0.025, 0.04, 0.08, 0.3, 0.1, 0.08],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['', '', '', '', '', '', ''],
+                     ['Open pasture',
+                      'Roads and Paths',
+                      'Tracks',
+                      'Trees',
+                      'Buildings',
+                      'Inland water',
+                      'Gardens'],
+                     [0, 1, 2, 3, 4, 5, 6]]
+        
+        row_list = mat.row_collection.getRowDataAsList()
+        self.assertListEqual(test_list, row_list, 'List contents not equal fail')
+    
+    
+    def test_createMatCsvDataFileObject(self):
+        """Loads a Materials.csv file and checks setup"""
+        
+        mat_path = os.path.join(self.curd, r'tests\test_data\materials_csv\Materials_CSV.csv')
+        hex_hash = hashlib.md5(mat_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, mat_path, hex_hash, 3, 'Read Materials File')
+        mat = datafileloader.loadDataFile(datafile)
+    
+        self.assertIsInstance(mat, MatCsvDataObject, 'Factory create MatCsvDataObject fail')
+        self.assertEqual(hex_hash, mat.path_holder.hex_hash, 'Hex_hash equality fail')
+        self.assertEqual('Read Materials File', mat.path_holder.command, 'Command equality fail')
+    
+        test_list = [['1', '2', '3', '4', '10', '11', '20', '21', '30', '31', '40'],
+                     ['', 0.022, '', 0.03, 0.08, '', '', '', '', '', ''],
+                     [0.03, '', 0.03, '', '', 0.03, '', '', '', '', ''],
+                     [0.1, '', 0.02, '', '', 0.1, '', '', '', '', ''],
+                     [0.1, '', 0.1, '', '', 0.1, '', '', '', '', ''],
+                     [0.06, '', 3.0, '', '', 0.04, '', '', '', '', ''],
+                     ['',
+                      '',
+                      '',
+                      '',
+                      '',
+                      '',
+                      'Otherfile.csv',
+                      'Otherfile.csv',
+                      'Anotherfile.csv',
+                      'Anotherfile.csv',
+                      'Onlyname.csv'],
+                     ['', '', '', '', '', '', 'Depth', 'Depth', '< 50mm', '50mm - 150mm', ''],
+                     ['', '', '', '', '', '', 'light values', 'heavy values', '', '', ''],
+                     [5.0, 0.0, 0.0, 5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     ['', '', '', '', '', '', '', '', '', '', ''],
+                     ['! Description',
+                      '!Pasture',
+                      '!Roads',
+                      '!Buildings',
+                      '!Ponds and other water',
+                      '!Vegetated creek',
+                      '!Maintained Grass',
+                      '!Subfile light test',
+                      '!Subfile heavy test',
+                      '!Anotherfile under 50 test',
+                      '!Anotherfile over 50 test',
+                      '!Onlyname test'],
+                     ['Material ID',
+                      "Manning's n",
+                      '',
+                      '',
+                      '',
+                      '',
+                      '',
+                      '',
+                      '',
+                      'Infiltration Parameters',
+                      '',
+                      'Land Us Hazard ID'],
+                     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
+        row_list = mat.row_collection.getRowDataAsList()
+        self.assertListEqual(test_list, row_list, 'List contents not equal fail')
+        
+        
+        '''
+         Check that the sub files loaded properly as well
+        '''
+        sub_extract = {}
+        for sub in mat.subfiles:
+            sub_extract[sub.file_name] = sub.row_collection.getRowDataAsList()
+            i=0
+            
+        name1 = 'Otherfile'
+        otherfile_testlist = [[0.03, 0.06, 0.09],
+                             [0.48, 0.31, 0.194],
+                             [0.8, 0.6, 0.36],
+                             ['', '', ''],
+                             ['', 'Some other text that noone cares about', ''],
+                             ['Depth', 'light values', 'heavy values', '', ''],
+                             [0, 1, 2, 3, 4]]
+    
+        name2 = 'Onlyname'
+        onlyname_testlist = [[0.002, 0.00309, 0.00331],
+                             [0.400786, 0.074916, 0.056848],
+                             ['Random header', ''],
+                             [0, 1, 2, 3, 4]]
+        
+        name3 = 'Anotherfile'
+        anotherfile_testlist = [[0.0477, 0.0077, 0.0132, 0.0277, 0.036],
+                                 [0.88, 0.765, 0.666, 0.646, 0.626],
+                                 ['', '', '', '', ''],
+                                 [0.0155, 0.0177, 0.0277, 0.0377, 0.0527],
+                                 [0.753, 0.74, 0.666, 0.631, 0.5947],
+                                 ['< 50mm', '', '', '50mm - 150mm', ''],
+                                 [0, 1, 2, 3, 4, 5, 6]]
+        
+        self.assertListEqual(otherfile_testlist, sub_extract[name1], 'Otherfile data fail')
+        self.assertListEqual(onlyname_testlist, sub_extract[name2], 'Onlyname data fail')
+        self.assertListEqual(anotherfile_testlist, sub_extract[name3], 'Anotherfile data fail')
+    
+    
+    
+    
---
+++ tests/test_datafileobject.py
@@ -0,0 +1,117 @@
+
+import unittest
+import os
+import hashlib
+
+from ship.tuflow.data_files import datafileloader
+from ship.tuflow.tuflowfilepart import DataFile, GisFile
+
+class DataFileObjectTests(unittest.TestCase):
+    """
+    """
+    
+    def setUp(self):
+        """
+        """
+        self.curd = os.getcwd()
+        self.comment_types = ['!', '#']
+        
+    
+    def test_BcGetPrintableContents(self):
+        """
+        """
+        bc_path = os.path.join(self.curd, r'tests\test_data\bc_dbase\bc_dbase_test.csv') 
+        hex_hash = hashlib.md5(bc_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, bc_path, hex_hash, 3, 'BC Database')
+        bc = datafileloader.loadDataFile(datafile)
+        
+        test_contents = ['Name, Source, Column1_or_Time, Column2_or_Value_or_ID, TimeAdd, ValueMult, ValueAdd, IMultF, IAddF',
+                         'Name1, Name1.csv, time, Downstream, , , , , ',
+                         'Name2, Name2.csv, time, Flow, , , , , ',
+                         'Name3, Name2.csv, time, Dummy, , , , , '
+                         ]
+        contents = bc._getPrintableContents()
+        self.assertListEqual(test_contents, contents, 'Output contents match fail')    
+    
+    
+    def test_TmfGetPrintableContents(self):
+        """
+        """
+        mat_path = os.path.join(self.curd, r'tests\test_data\Materials_TMF.tmf') 
+        hex_hash = hashlib.md5(mat_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, mat_path, hex_hash, 3, 'Read Materials File')
+        mat = datafileloader.loadDataFile(datafile)
+        
+        test_contents = ['! Materials file test.',
+                        '!',
+                        '! Some comments at the top',
+                        '',
+                        '1, 0.060, , , , , , , , ,  ! Open pasture',
+                        '2, 0.025, , , , , , , , ,  ! Roads and Paths',
+                        '3, 0.040, , , , , , , , ,  ! Tracks',
+                        '4, 0.080, , , , , , , , ,  ! Trees',
+                        '5, 0.300, , , , , , , , ,  ! Buildings',
+                        '6, 0.100, , , , , , , , ,  ! Inland water',
+                        '7, 0.080, , , , , , , , ,  ! Gardens',
+                        '']
+        contents = mat._getPrintableContents()
+        self.assertListEqual(test_contents, contents, 'Output contents match fail')
+         
+
+    def test_MatCsvGetPrintableContents(self):
+        """Check the contents of Materials csv file ok"""
+        mat_path = os.path.join(self.curd, r'tests\test_data\materials_csv\Materials_CSV.csv') 
+        hex_hash = hashlib.md5(mat_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, mat_path, hex_hash, 3, 'Read Materials File')
+        mat = datafileloader.loadDataFile(datafile)
+        
+        test_contents = ["Material ID, Manning's n, Infiltration Parameters, Land Us Hazard ID, ! Description",
+                         '1,"0.030,0.100,0.100,0.060","5.000, 2.000",,!Pasture',
+                         '2,0.022,"0.000, 2.000",,!Roads',
+                         '3,"0.030,0.020,0.100,3.000","0.000, 2.000",,!Buildings',
+                         '4,0.030,"5.000, 2.000",,!Ponds and other water',
+                         '10,0.080,"5.000, 2.000",,!Vegetated creek',
+                         '11,"0.030,0.100,0.100,0.040","5.000, 2.000",,!Maintained Grass',
+                         '20,Otherfile.csv | Depth | light values,"0.000, 0.000",,!Subfile light test',
+                         '21,Otherfile.csv | Depth | heavy values,"0.000, 0.000",,!Subfile heavy test',
+                         '30,Anotherfile.csv | < 50mm,"0.000, 0.000",,!Anotherfile under 50 test',
+                         '31,Anotherfile.csv | 50mm - 150mm,"0.000, 0.000",,!Anotherfile over 50 test',
+                         '40,Onlyname.csv,"0.000, 0.000",,!Onlyname test']
+        contents = mat._getPrintableContents()
+        self.assertListEqual(test_contents, contents, 'Output contents match fail')
+        
+    
+    def test_MatCsvSubfileGetPrintableContents(self):
+        """Checks the contents of subfile is working properly"""
+        mat_path = os.path.join(self.curd, r'tests\test_data\materials_csv\Materials_CSV.csv') 
+        hex_hash = hashlib.md5(mat_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = DataFile(1, mat_path, hex_hash, 3, 'Read Materials File')
+        mat = datafileloader.loadDataFile(datafile)
+        
+        test_contents = ['Depth, light values, heavy values, , ',
+                         'y (m) ,      n ,      n ,       ,      ',
+                         ' 0.030000, 0.480000, 0.800000, , ',
+                         ' 0.060000, 0.310000, 0.600000, , Some other text that noone cares about',
+                         ' 0.090000, 0.194000, 0.360000, , ']
+        contents = mat.subfiles[0]._getPrintableContents()
+        self.assertListEqual(test_contents, contents, 'Output contents match fail')
+        
+        
+    def test_XsGetPrintableContents(self):
+        """Check that this is not implemented."""
+        gis_path = os.path.join(self.curd, r'tests\test_data\xs\1d_xs_test.mif') 
+        hex_hash = hashlib.md5(gis_path.encode())
+        hex_hash = hex_hash.hexdigest()
+        datafile = GisFile(1, gis_path, hex_hash, 3, 'Read MI Table Links')
+        gis = datafileloader.loadDataFile(datafile)
+
+        self.assertRaises(NotImplementedError, lambda: gis._getPrintableContents())
+        self.assertRaises(NotImplementedError, lambda: gis.saveData())
+        
+        
+    
+        
